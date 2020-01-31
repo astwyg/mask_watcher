2a3589f8795b089a0446d58586ca90e34a09575e
WANG. YG <astwyg@126.com>
2020-01-31 11:57:12 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mask_watcher
 口罩到货监测
+
+# 使用方法
+
+1. 填写`conifg/mail_config.example.py`, 并重命名该文件, 去掉example
+2. 运行`src/watcher.py`
---
+++ src/watcher.py
@@ -0,0 +1,43 @@
+import os, time, json
+from selenium import webdriver
+from log.logger import logger as log
+from src.mail import send_mail
+from config.mail_config import mail_config
+
+browser = None
+
+def check_shop(url, keywords):
+    browser.get(url)
+    time.sleep(5)
+    find_flag = False
+    for keyword in keywords:
+        if keyword in browser.page_source:
+            find_flag = keyword
+            break
+    if not find_flag:
+        log.warning("FIND!!!")
+        log.warning(url)
+        log.warning(keywords)
+        send_mail(
+            "发现口罩有货!!",
+            url,
+            mail_config.get("to")
+        )
+
+
+
+def check_all_shops():
+    with open(os.path.join(os.path.dirname(__file__),"..","config","shop.json"), "r", encoding='UTF-8') as f:
+        infos = json.loads(f.read())
+        for info in infos:
+            for shop in info["shop"]:
+                log.info("checking {} / {}".format(shop, info.get("keyword")))
+                keywords = info.get("key_word").split(",")
+                check_shop(shop, keywords)
+
+
+if __name__ == "__main__":
+    while True:
+        browser = webdriver.Chrome(os.path.join(os.path.dirname(__file__), "chromedriver.exe"))
+        check_all_shops()
+        browser.quit()
